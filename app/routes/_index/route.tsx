529d161d0dbc9b7d3f05b401238068fe5e870d1b
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-04-02 22:49:12 -0400

    Added temporary about and resume sections

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,5 +1,10 @@
 import type { MetaFunction } from '@remix-run/node';
-import { MAIN_HEADING_TEXT, MAIN_SUBHEADING_TEXT } from './constants';
+import {
+    ABOUT_TEXT,
+    MAIN_HEADING_TEXT,
+    MAIN_SUBHEADING_TEXT,
+    RESUME_TEXT,
+} from './constants';
 
 export const meta: MetaFunction = function () {
     return [
@@ -17,6 +22,15 @@ export default function Index() {
         <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
             <h1 className="text-6xl font-bold">{MAIN_HEADING_TEXT}</h1>
             <h3 className="pt-5 text-2xl">{MAIN_SUBHEADING_TEXT}</h3>
+
+            <a href="/#about">
+                <h3 className="pt-16 text-xl">{ABOUT_TEXT}</h3>
+            </a>
+            <a href="/#resume">
+                <h3 className="text-xl">{RESUME_TEXT}</h3>
+            </a>
+            <p id="about">About</p>
+            <p id="resume">Testing</p>
         </div>
     );
 }
