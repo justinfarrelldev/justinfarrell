b758e7be8c2ad497944ebf6395dfd15aa549411f
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-04-16 10:24:34 -0400

    Added margin to the skill badges and an explanatory comment

--- app/routes/_index/skillBadge.tsx
+++ app/routes/_index/skillBadge.tsx
@@ -14,18 +14,20 @@ export const SkillBadge: FC<Props> = function ({
     skill,
     confidence,
 }: Props): React.ReactElement {
+    // This might be a little sloppy, but it gives the intended effect and I'm too tired this morning to really worry!
+    // I am aware there are better ways and that DRY is being criminally violated here...
     let badgeClass: string;
     let tooltipClass: string;
     if (confidence === 'expert') {
-        badgeClass = 'has-tooltip badge badge-success';
+        badgeClass = 'has-tooltip badge badge-success mr-2';
         tooltipClass =
             'tooltip bottom-full w-52 rounded bg-success p-1 text-left';
     } else if (confidence === 'confident') {
-        badgeClass = 'has-tooltip badge badge-warning';
+        badgeClass = 'has-tooltip badge badge-warning mr-2';
         tooltipClass =
             'tooltip bottom-full w-52 rounded bg-warning p-1 text-left';
     } else {
-        badgeClass = 'has-tooltip badge badge-error';
+        badgeClass = 'has-tooltip badge badge-error mr-2';
         tooltipClass =
             'tooltip bottom-full w-52 rounded bg-error p-1 text-left';
     }
