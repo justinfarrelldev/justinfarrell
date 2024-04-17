e1f49a4c0b3932373d74445e58a8f36897f4e8c0
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-04-17 01:04:08 -0400

    Added an alert as a disclaimer

--- app/routes/_index/skillsBadges.tsx
+++ app/routes/_index/skillsBadges.tsx
@@ -279,10 +279,32 @@ export const SkillBadges: FC = function () {
     const [searchText, setSearchText] = useState<string>();
     return (
         <>
+            <div role="alert" className="alert alert-info my-4 p-1">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="white"
+                    className="ml-2 size-8"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+                    />
+                </svg>
+
+                <span className="cursor-default text-lg">
+                    Pardon the dust - this section is still under active
+                    construction! This is also not an exhaustive list of my
+                    skill set as of right now, but hopefully it will be soon!
+                </span>
+            </div>
             <input
                 type="text"
                 placeholder="Search for a skill"
-                className="input input-bordered my-4 w-full"
+                className="input input-bordered input-secondary my-4 w-full"
                 onChange={function (event) {
                     setSearchText(event.target.value);
                 }}
