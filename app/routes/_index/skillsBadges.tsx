78c172d919d4f1b95a77751be14818f2c0ccd56c
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-05-02 02:12:59 -0400

    Made the alert info box into a component

--- app/routes/_index/skillsBadges.tsx
+++ app/routes/_index/skillsBadges.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useState } from 'react';
 import { Skill, SkillBadge } from './skillBadge';
+import { Alert } from './alert';
 
 export const badgeArray: React.JSX.Element[] = [
     <SkillBadge
@@ -311,28 +312,11 @@ export const SkillBadges: FC = function () {
     const [searchText, setSearchText] = useState<string>();
     return (
         <>
-            <div role="alert" className="alert alert-info my-4 p-1">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="white"
-                    className="ml-2 size-8"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
-                    />
-                </svg>
-
-                <span className="cursor-default text-lg">
-                    Pardon the dust - this section is still under active
-                    construction! This is also not an exhaustive list of my
-                    skill set as of right now, but hopefully it will be soon!
-                </span>
-            </div>
+            <Alert
+                text="Pardon the dust - this section is still under active
+                construction! This is also not an exhaustive list of my skill
+                set as of right now, but hopefully it will be soon!"
+            />
             <input
                 type="text"
                 placeholder="Search for a skill"
