8fde887fb5a361baef7161ff8a5cf886a250673f
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-08-23 22:05:31 -0400

    Added tests for the rest of the sections

--- app/routes/_index/route.test.tsx
+++ app/routes/_index/route.test.tsx
@@ -11,8 +11,11 @@ import { cleanup, render, screen } from '@testing-library/react';
 import Index from './route';
 import {
     ABOUT_LINK_TEXT,
+    EXPERIENCE_LINK_TEXT,
+    INQUIRE_LINK_TEXT,
     MAIN_HEADING_TEXT,
     MAIN_SUBHEADING_TEXT,
+    SKILLS_LINK_TEXT,
 } from './constants';
 import { createRemixStub } from '@remix-run/testing';
 
@@ -47,11 +50,32 @@ describe('_index route', function () {
 
         expect(screen.queryByText(MAIN_SUBHEADING_TEXT)).toBeInTheDocument();
     });
-    it(`should contain the text ${ABOUT_LINK_TEXT} twice - one for the navigation and one for the element`, function () {
+    it(`should contain the text "${ABOUT_LINK_TEXT}" twice - one for the navigation and one for the element`, function () {
         render(<RemixStub />);
 
         expect(
             screen.queryAllByText(ABOUT_LINK_TEXT, { exact: true })
+        ).toHaveLength(2);
+    });
+    it(`should contain the text "${INQUIRE_LINK_TEXT}" twice - one for the navigation and one for the element`, function () {
+        render(<RemixStub />);
+
+        expect(
+            screen.queryAllByText(INQUIRE_LINK_TEXT, { exact: true })
+        ).toHaveLength(2);
+    });
+    it(`should contain the text "${EXPERIENCE_LINK_TEXT}" twice - one for the navigation and one for the element`, function () {
+        render(<RemixStub />);
+
+        expect(
+            screen.queryAllByText(EXPERIENCE_LINK_TEXT, { exact: true })
+        ).toHaveLength(2);
+    });
+    it(`should contain the text "${SKILLS_LINK_TEXT}" twice - one for the navigation and one for the element`, function () {
+        render(<RemixStub />);
+
+        expect(
+            screen.queryAllByText(SKILLS_LINK_TEXT, { exact: true })
         ).toHaveLength(2);
     });
 });
