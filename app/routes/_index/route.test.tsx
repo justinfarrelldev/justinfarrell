2c31626a1b61f4070ce935b09fc00935247bf175
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-08-23 22:03:38 -0400

    Added test to check for the presence of the "About Me" section

--- app/routes/_index/route.test.tsx
+++ app/routes/_index/route.test.tsx
@@ -9,7 +9,11 @@ import {
 } from 'vitest';
 import { cleanup, render, screen } from '@testing-library/react';
 import Index from './route';
-import { MAIN_HEADING_TEXT, MAIN_SUBHEADING_TEXT } from './constants';
+import {
+    ABOUT_LINK_TEXT,
+    MAIN_HEADING_TEXT,
+    MAIN_SUBHEADING_TEXT,
+} from './constants';
 import { createRemixStub } from '@remix-run/testing';
 
 const RemixStub = createRemixStub([
@@ -42,5 +46,12 @@ describe('_index route', function () {
         render(<RemixStub />);
 
         expect(screen.queryByText(MAIN_SUBHEADING_TEXT)).toBeInTheDocument();
+    });
+    it(`should contain the text ${ABOUT_LINK_TEXT} twice - one for the navigation and one for the element`, function () {
+        render(<RemixStub />);
+
+        expect(
+            screen.queryAllByText(ABOUT_LINK_TEXT, { exact: true })
+        ).toHaveLength(2);
     });
 });
